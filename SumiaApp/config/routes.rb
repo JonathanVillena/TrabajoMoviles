189cf83a6c42e5b958df94886374bd9ba9dc4211
Jonathan Villena Vilca <jonathanvillena@gmail.com>
2016-10-16 00:59:01 +0000

    Admin,user intalado

--- SumiaApp/config/routes.rb
+++ SumiaApp/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  namespace :api do
+    scope :v1 do
+       mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
   resources :tags
   resources :bags
   resources :dreams
